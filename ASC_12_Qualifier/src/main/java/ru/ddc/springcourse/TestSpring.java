fb25075adc682b563c32cab3eead4552db8638df
Sergey Kovalev <dipdeepcode@mail.ru>
2023-09-07 20:09:44 +0300

    ASC_12_Qualifier :: добавлен модуль

---
+++ ASC_12_Qualifier/src/main/java/ru/ddc/springcourse/TestSpring.java
@@ -0,0 +1,15 @@
+package ru.ddc.springcourse;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestSpring {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        MusicPlayer player = context.getBean("musicPlayer", MusicPlayer.class);
+        player.playMusic();
+
+        context.close();
+    }
+}
