a3136ce940e7500d389343eacf3b27a0718de940
Sergey Kovalev <dipdeepcode@mail.ru>
2023-09-08 22:44:20 +0300

    ASC_25_Validation :: добавлен модуль

---
+++ ASC_25_Validation/src/main/java/ru/ddc/springcourse/dao/PersonDao.java
@@ -0,0 +1,44 @@
+package ru.ddc.springcourse.dao;
+
+import org.springframework.stereotype.Component;
+import ru.ddc.springcourse.model.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDao {
+    private static int PEOPLE_COUNT;
+    private final List<Person> people = new ArrayList<>();
+
+    {
+        people.add(new Person(++PEOPLE_COUNT, "Tom", 24, "tom@mail.ru"));
+        people.add(new Person(++PEOPLE_COUNT, "Bob", 52, "bob@mail.ru"));
+        people.add(new Person(++PEOPLE_COUNT, "Mike", 18, "mike@mail.ru"));
+        people.add(new Person(++PEOPLE_COUNT, "Katy", 34, "katy@mail.ru"));
+    }
+
+    public List<Person> index() {
+        return people;
+    }
+
+    public Person show(int id) {
+        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+    }
+
+    public void save(Person person) {
+        person.setId(++PEOPLE_COUNT);
+        people.add(person);
+    }
+
+    public void update(int id, Person person) {
+        Person personToBeUpdated = show(id);
+        personToBeUpdated.setName(person.getName());
+        personToBeUpdated.setAge(person.getAge());
+        personToBeUpdated.setEmail(person.getEmail());
+    }
+
+    public void delete(int id) {
+        people.removeIf(person -> person.getId() == id);
+    }
+}
