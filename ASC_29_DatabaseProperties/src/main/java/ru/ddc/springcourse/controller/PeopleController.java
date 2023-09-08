21668d4238e565adccc8f6dbbe208f07b9a6074d
Sergey Kovalev <dipdeepcode@mail.ru>
2023-09-08 22:52:01 +0300

    ASC_29_DatabaseProperties :: добавлен модуль

---
+++ ASC_29_DatabaseProperties/src/main/java/ru/ddc/springcourse/controller/PeopleController.java
@@ -0,0 +1,74 @@
+package ru.ddc.springcourse.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import ru.ddc.springcourse.dao.PersonDao;
+import ru.ddc.springcourse.model.Person;
+
+import javax.validation.Valid;
+
+
+@Controller
+@RequestMapping("/people")
+public class PeopleController {
+
+    private final PersonDao personDao;
+
+    public PeopleController(PersonDao personDao) {
+        this.personDao = personDao;
+    }
+
+    @GetMapping
+    public String index(Model model) {
+        model.addAttribute("people", personDao.index());
+        return "people/index";
+    }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") int id, Model model) {
+        model.addAttribute("person", personDao.show(id));
+        return "people/show";
+    }
+
+    @GetMapping("/new")
+    public String newPerson(@ModelAttribute("person") Person person) {
+        return "people/new";
+    }
+
+    @PostMapping
+    public String create(@ModelAttribute("person") @Valid Person person,
+                         BindingResult result) {
+        if (result.hasErrors()) {
+            return "people/new";
+        } else {
+            personDao.save(person);
+            return "redirect:/people";
+        }
+    }
+
+    @GetMapping("/{id}/edit")
+    public String edit(@PathVariable("id") int id, Model model) {
+        model.addAttribute("person", personDao.show(id));
+        return "people/edit";
+    }
+
+    @PatchMapping("/{id}")
+    public String update(@PathVariable("id") int id,
+                         @ModelAttribute("person") @Valid Person person,
+                         BindingResult result) {
+        if (result.hasErrors()) {
+            return "people/edit";
+        } else {
+            personDao.update(id, person);
+            return "redirect:/people";
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable("id") int id) {
+        personDao.delete(id);
+        return "redirect:/people";
+    }
+}
