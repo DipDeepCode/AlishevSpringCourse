9e140d5e534535dbb5803e5163bc78f4375049e2
Sergey Kovalev <dipdeepcode@mail.ru>
2023-09-08 23:31:06 +0300

    ASC_49_SaveToBD :: добавлен модуль

---
+++ ASC_49_SaveToBD/src/main/java/ru/ddc/App.java
@@ -0,0 +1,30 @@
+package ru.ddc;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import ru.ddc.model.Person;
+
+public class App {
+    public static void main(String[] args) {
+        Configuration configuration = new Configuration().addAnnotatedClass(Person.class);
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session session = sessionFactory.getCurrentSession();
+
+        try {
+            session.beginTransaction();
+
+            Person person1 = new Person("Test1", 30);
+            Person person2 = new Person("Test2", 40);
+            Person person3 = new Person("Test3", 50);
+
+            session.save(person1);
+            session.save(person2);
+            session.save(person3);
+
+            session.getTransaction().commit();
+        } finally {
+            sessionFactory.close();
+        }
+    }
+}
