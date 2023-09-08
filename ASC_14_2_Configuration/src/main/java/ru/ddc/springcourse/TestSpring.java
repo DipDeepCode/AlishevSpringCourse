56928a651d50da6b084b97bc695733958f28e989
Sergey Kovalev <dipdeepcode@mail.ru>
2023-09-08 22:01:47 +0300

    ASC_14_2_Configuration :: добавлен модуль

---
+++ ASC_14_2_Configuration/src/main/java/ru/ddc/springcourse/TestSpring.java
@@ -0,0 +1,23 @@
+package ru.ddc.springcourse;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class TestSpring {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);
+
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        System.out.println(musicPlayer.getName());
+        System.out.println(musicPlayer.getVolume());
+
+        ClassicalMusic classicalMusic1 = context.getBean("classicalMusic", ClassicalMusic.class);
+        ClassicalMusic classicalMusic2 = context.getBean("classicalMusic", ClassicalMusic.class);
+        System.out.println(classicalMusic1 == classicalMusic2);
+
+//        RockMusic rockMusic1 = context.getBean("rockMusic", RockMusic.class);
+//        RockMusic rockMusic2 = context.getBean("rockMusic", RockMusic.class);
+//        System.out.println(rockMusic1 == rockMusic2);
+
+        context.close();
+    }
+}
