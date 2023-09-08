c6fc857b24f38e24f4072e4d452432d247c09d91
Sergey Kovalev <dipdeepcode@mail.ru>
2023-09-08 22:59:08 +0300

    ASC_41_1_SpringValidator :: добавлен модуль

---
+++ ASC_41_1_SpringValidator/src/main/java/ru/ddc/springcourse/utils/PersonValidator.java
@@ -0,0 +1,30 @@
+package ru.ddc.springcourse.utils;
+
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+import ru.ddc.springcourse.dao.PersonDao;
+import ru.ddc.springcourse.model.Person;
+
+@Component
+public class PersonValidator implements Validator {
+    private final PersonDao personDao;
+
+    public PersonValidator(PersonDao personDao) {
+        this.personDao = personDao;
+    }
+
+    @Override
+    public boolean supports(@NonNull Class<?> clazz) {
+        return Person.class.equals(clazz);
+    }
+
+    @Override
+    public void validate(@NonNull Object target, @NonNull Errors errors) {
+        Person person = (Person) target;
+        if (personDao.show(person.getEmail()).isPresent()) {
+            errors.rejectValue("email", "", "This email is already taken");
+        }
+    }
+}
