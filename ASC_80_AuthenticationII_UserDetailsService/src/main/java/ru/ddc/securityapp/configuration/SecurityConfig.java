a91e14b58f5e90f3bfa5a47cd56b90e287bd0e9a
Sergey Kovalev <dipdeepcode@mail.ru>
2023-10-10 22:16:33 +0300

    ASC_80_AuthenticationII_UserDetailsService :: добавлен модуль

---
+++ ASC_80_AuthenticationII_UserDetailsService/src/main/java/ru/ddc/securityapp/configuration/SecurityConfig.java
@@ -0,0 +1,30 @@
+package ru.ddc.securityapp.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import ru.ddc.securityapp.service.PersonDetailsService;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final PersonDetailsService personDetailsService;
+
+    public SecurityConfig(PersonDetailsService personDetailsService) {
+        this.personDetailsService = personDetailsService;
+    }
+
+    // Настраивает аутентификацию
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(personDetailsService);
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
---
+++ ASC_80_AuthenticationII_UserDetailsService/src/main/java/ru/ddc/securityapp/security/PersonDetails.java
@@ -0,0 +1,49 @@
+package ru.ddc.securityapp.security;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import ru.ddc.securityapp.model.Person;
+
+import java.util.Collection;
+
+public record PersonDetails(Person person) implements UserDetails {
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return person.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return person.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+}
